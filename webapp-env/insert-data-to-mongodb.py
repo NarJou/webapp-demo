d1a719a5440241c0e3c21a99a1831e23eb1e6fe9
Sofiane FLIH <flih.sofiane@gmail.com>
2018-01-04 12:33:14 +0100

    Changed scripts to insert the whole product list

--- webapp-env/insert-data-to-mongodb.py
+++ webapp-env/insert-data-to-mongodb.py
@@ -4,21 +4,22 @@ from pymongo import MongoClient
 client = MongoClient('mongo:27017')
 db = client.test.products
 
-with open('ten-products.json', 'r') as f:
-  products = json.load(f)
+print "Loading entries in the Mongo DB\n"
 
-for product in products:
-  db.insert_one({
-    "sku":product.get("sku", None),
-    "name":product.get("name", None),
-    "type":product.get("type", None),
-    "price":product.get("price", None),
-    "upc":product.get("upc", None),
-    "category":product.get("category", None),
-    "shipping":product.get("shipping", None),
-    "description":product.get("description", None),
-    "manufacturer":product.get("manufacturer", None),
-    "model":product.get("model", None),
-    "url":product.get("url", None),
-    "image":product.get("image", None)
-  })
+with open('products.json', 'r') as f:
+    products = json.load(f)
+    for product in products:
+        db.insert_one({
+            "sku":product.get("sku", None),
+            "name":product.get("name", None),
+            "type":product.get("type", None),
+            "price":product.get("price", None),
+            "upc":product.get("upc", None),
+            "category":product.get("category", None),
+            "shipping":product.get("shipping", None),
+            "description":product.get("description", None),
+            "manufacturer":product.get("manufacturer", None),
+            "model":product.get("model", None),
+            "url":product.get("url", None),
+            "image":product.get("image", None)
+            })
